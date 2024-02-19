47a775695eb40375b883c76d6fd95813b15662a0
dikgobe <108192458+dikgobe@users.noreply.github.com>
2024-02-19 13:41:15 +0200

    Add files via upload

---
+++ Analytics.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Analytics";
+import python from './Python-Symbol.png'
+
+const Analytics = () => {
+  return (
+    <div>
+      <h1 className="projects-page-heading">Projects page</h1>
+      
+       <img src={python} alt="Python Logo" /> {/* Using the imported image */}
+       <p> then best degrree in te world</p>
+    
+    </div>
+  );
+};
+
+export default Analytics;
